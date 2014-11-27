0a552163b995820316579fa629e9264d4d658166
Aaron McGrath <aaron.mcgrath@adaptris.com>
2014-11-27 14:55:12 +0000

    INTERLOK-270 #time 30m #comment Added logging and changed the replace method for one that does not use regex.

--- src/com/adaptris/core/varsub/SimpleStringSubstitution.java
+++ src/com/adaptris/core/varsub/SimpleStringSubstitution.java
@@ -3,13 +3,21 @@ package com.adaptris.core.varsub;
 import java.util.Properties;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class SimpleStringSubstitution implements VariableSubstitutable {
+  
+  private transient Logger log = LoggerFactory.getLogger(this.getClass());
 
   @Override
   public String doSubstitution(String input, Properties variableSubs, String variablePrefix, String variablePostFix) {
     Set<Object> keySet = variableSubs.keySet();
+    log.trace("About to start configuration variable substitution");
     for(Object key : keySet) {
-      input = input.replaceAll(variablePrefix + key + variablePostFix, variableSubs.getProperty((String) key));
+      log.trace("Replacing " + variablePrefix + key + variablePostFix + " with " + variableSubs.getProperty((String) key));
+      input = input.replace(variablePrefix + key + variablePostFix, variableSubs.getProperty((String) key));
     }
     
     return input;
