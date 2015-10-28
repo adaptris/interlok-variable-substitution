52a769d1470e4d778609c12a07edd6f7f6b87ffe
Lewin Chan <lewin.chan@adaptris.com>
2015-10-28 15:30:13 +0000

    INTERLOK-696 Restored lower+camel enums INTERLOK-696 #time 5m
    Restored the lowercase+camel enums but marked them as deprecated so
    that we can still use variable-substitution.impl=simpleWithLogging
    in bootstrap.properties.
    
    It's also a good test for INTERLOK-669 when adding a dynamic service
    locator with an xstream-varsub-marshaller...

--- src/com/adaptris/core/varsub/VariableSubstitutionType.java
+++ src/com/adaptris/core/varsub/VariableSubstitutionType.java
@@ -9,7 +9,19 @@ public enum VariableSubstitutionType {
       return new SimpleStringSubstitution();
     }
   },
+  @Deprecated simple() {
+    @Override
+    VariableSubstitutable create() {
+      return new SimpleStringSubstitution();
+    }
+  },
   SIMPLE_WITH_LOGGING() {
+    @Override
+    VariableSubstitutable create() {
+      return new SimpleStringSubstitution(true, false);
+    }
+  },
+  @Deprecated simpleWithLogging() {
     @Override
     VariableSubstitutable create() {
       return new SimpleStringSubstitution(true, false);
@@ -21,7 +33,19 @@ public enum VariableSubstitutionType {
       return new SimpleStringSubstitution(false, true);
     }
   },
+  @Deprecated strict() {
+    @Override
+    VariableSubstitutable create() {
+      return new SimpleStringSubstitution(false, true);
+    }
+  },
   STRICT_WITH_LOGGING() {
+    @Override
+    VariableSubstitutable create() {
+      return new SimpleStringSubstitution(true, true);
+    }
+  },
+  @Deprecated stringWithLogging() {
     @Override
     VariableSubstitutable create() {
       return new SimpleStringSubstitution(true, true);
